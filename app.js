731f50d50cfb5d0663becada62ef02a5712937b3
Lorena Citlalli Galindo Gonzalez <230598@utxicotepec.edu.mx>
2025-01-16 15:30:57 -0600

    Practica03
    Se realizan cambios ejecutados

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ app.use(session({
     cookie: { secure: false, maxAge: 24 * 60 * 60 * 100}//usar secure:true solo si usas Https,maxAge permite  
 }));
 
+
+
+//Ruta para inicializar la sesion 
+app.get('/login/:name', (req ,res) =>{
+    const userName = req.params.name;
+    if(!req.session.createAt){
+        req.session.userName = userName;
+        req.session.createAt= new Date();
+        req.session.lastAcess= new Date ();
+
+        res.send(`
+            <h1>Bienvenido, tu sesión ha sido iniciada</h1>
+            <p><strong>Nombre de usuario: </strong> ${userName}</p>
+            `)
+    }else{
+        res.send('<h1>Ya existe una sesión</h1>')
+    }
+})
+
 app.get('/update', (req, res) =>{
     if(req.session.createAt){
         req.session.lastAcess = new Date();
@@ -21,44 +40,25 @@ app.get('/update', (req, res) =>{
     }
 })
 
-//Ruta para inicializar la sesion 
-app.get('/login/:name', (req ,res) =>{
-    const userName = req.params.name;
-    if(!req.session.createAt){
-        req.session.userName = userName;
-        req.session.createAt= new Date();
-        req.session.lastAcess= new Date ();
-        //req.session.createAt = new Date();
-        //req.session.lastAcess= new Date();
-        res.send(`
-            <h1>Bienvenido, tu sesión ha sido iniciada</h1>
-            <p><strong>Nombre de usuario: </strong> ${userName}</p>
-            <p><a href="/session">Ir a detalles de la sesión</a></p>
-            `)
-    }else{
-        res.send('<h1>Ya existe una sesión</h1>')
-    }
-})
-
 //Ruta para para el estado de la sesion
 app.get('/status',(req,res)=>{
-    if (!req.session.createdAt){
+    if (req.session.createAt){
         const now =new Date();
-        const started= new Date(req.session.createdAt);
-        const lastUpdate=new Date(req.session.lastAcces);
+        const started= new Date(req.session.createAt);
+        const lastUpdate=new Date(req.session.lastAcess);
         //calcular antiguedad de la sesion 
         const sessionAgeMs=now -started;
-        const hours= Math.floor(sessionAgeMs/(1000*60*60));
-        const minutes= Math.floor(sessionAgeMs/(1000*60*60)/(1000*60));
-        const seconds= Math.floor(sessionAgeMs/(1000*60)/1000);
+        const hours= Math.floor (sessionAgeMs/(1000*60*60));
+        const minutes= Math.floor ((sessionAgeMs % (1000*60*60))/(1000*60));
+        const seconds= Math.floor ((sessionAgeMs % (1000*60))/1000);
         //converir las fechas al uso horario de CDMX
-        const createdAt_CDMX=moment(started).tz('America/MExico_City').format('YYYY-MM-DD HH:mm:ss');
-        const lastAcces_CDMX=moment(lastUpdate).tz('America/MExico_City').format('YYYY-MM-DD HH:mm:ss');
+        const createAt_CDMX=moment(started).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+        const lastAcces_CDMX=moment(lastUpdate).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
 
         res.json({
             mensaje: 'Estado de la sesion',
             sessionId:  req.sessionID,
-            inicio: createdAt_CDMX,
+            inicio: createAt_CDMX,
             ultimoAcceso: lastAcces_CDMX,
             antiguedad: `${hours} horas, ${minutes} minutes y ${seconds} segundos`
         });
@@ -86,12 +86,10 @@ app.get('/session', (req, res)=>{
         <p><strong>Fecha de creación de la sesión:</strong> ${createAt}</p>
         <p><strong>Último acceso:</strong> ${lastAcess}</p>
         <p><strong>Duración de la sesión (en segundos): </strong> ${sessionDuration}</p>
-        <p><a href="/logout">Cerrar sesión</a></p>
         `)
     }else{
         res.send(`
             <h1>No hay sesión activa.</h1>
-            <p><a href="/login/Invitado">Iniciar sesión como Invitado</a></p>
             `);
         }
     });
@@ -106,7 +104,7 @@ app.get('/logout', (req, res) => {
             res.send('<h1>Sesión cerrada exitosamente.</h1>')
         })
     }else{
-        res.send('No hay una sesión activca para cerrar')
+        res.send('No hay una sesión activa para cerrar')
     }
 
 })
