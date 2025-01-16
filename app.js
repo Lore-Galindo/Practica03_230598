ae94d7bb7066b2b59305664d5a3b949b2905fb44
Lorena Citlalli Galindo Gonzalez <230598@utxicotepec.edu.mx>
2025-01-16 09:59:45 -0600

    Practica04
    Se realizo cambios

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 const express = require('express');
 const session = require('express-session');
+const moment = require('moment-timezone');// commentJS
 
 const app = express();
 
 // Configuración de la sesión
 app.use(session({
-    secret: 'mi-clave-secreta',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
+    secret: 'p3-LCGG #chompy10-Sesionespersistentes',
+    resave: false,           //No guarda la sesion si no ha sido modificada 
+    saveUninitialized: true, //Guarda la sesion aun que no haya sisdo inicializada 
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 100}//usar secure:true solo si usas Https,maxAge permite  
 }));
+//Ruta para inicializar la sesion 
+app.get ('/login/',(req, res)=>{
+    if (!req.session.createdAt){
+        req.session.createdAt=new Date();
+        req.session.lastAcces=new Date();
+        res.send('La sesion ha sido inicializada.')
+    }else{
+        res.send('Ya existe una sesion')
+    }
+});
+//Ruta para actualizar la fecha de la ultima consulta 
+app.get('update',(req, res)=>{
+    if (!req.session.createdAt){
+        req.session.createdAt=new Date();
+        res.send('La fecha de ultimo acceso ha sido actuualizada.')
+    }else{
+        res.send('No hay una sesion activa ')
+    }
+
+});
+//Ruta para para el estado de la sesion
+app.get('/status',(req,res)=>{
+    if (!req.session.createdAt){
+        const now =new Date();
+        const started= new Date(req.session.createdAt);
+        const lastUpdate=new Date(req.session.lastAcces);
+        //calcular antiguedad de la sesion 
+        const sessionAgeMs=now -started;
+        const hours= Math.floor(sessionAgeMs/(1000*60*60));
+        const minutes= Math.floor(sessionAgeMs/(1000*60*60)/(1000*60));
+        const seconds= Math.floor(sessionAgeMs/(1000*60)/1000);
+        //converir las fechas al uso horario de CDMX
+        const createdAt_CDMX=moment(started).tz('America/MExico_City').format('YYYY-MM-DD HH:mm:ss');
+        const lastAcces_CDMX=moment(lastUpdate).tz('America/MExico_City').format('YYYY-MM-DD HH:mm:ss');
+
+        res.json({
+            mensaje: 'Estado de la sesion',
+            sessionId:  req.sessionID,
+            inicio: createdAt_CDMX,
+            ultimoAcceso: lastAcces_CDMX,
+            antiguedad: `${hours} horas, ${minutes} minutes y ${seconds} segundos`
+        });
+    }else{
+        res.send('No hay una sesion activa')
+    }
+});
+
+//Ruta para cerrar la sesion
+a
 
 // Middleware para mostrar detalles de la sesión
 app.use((req, res, next) => {
